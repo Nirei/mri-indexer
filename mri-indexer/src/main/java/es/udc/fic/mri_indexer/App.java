0c0f2557ae85dbdbe8a94d8da2e9fdbe1b0db098
nirro <nirro@localhost.localdomain>
2017-02-15 13:42:20 +0100

    Primer commit.

---
+++ mri-indexer/src/main/java/es/udc/fic/mri_indexer/App.java
@@ -0,0 +1,45 @@
+package es.udc.fic.mri_indexer;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+    final static String usage = "java es.udc.fic.mri_indexer.IndexFiles"
+    		+ " -index INDEX_PATH -coll DOC_PATH";
+
+    public static void main( String[] args )
+    {
+    	String index = null;
+    	String docs = null;
+    	
+    	for(int i=0; i<args.length; i++) {
+    		if(args[i].startsWith("-index")) {
+    			index = args[++i];
+    		}
+    		else if(args[i].startsWith("-coll")) {
+    			docs = args[++i];
+    		}
+    	}
+    	
+    	if(index == null
+    			|| docs == null) {
+			System.err.println(usage);
+			System.exit(1);
+    	}
+
+    	final Path indexDir = Paths.get(index);
+        final Path docDir = Paths.get(docs);
+        if (!Files.isReadable(docDir)) {
+          System.out.println("Document directory '" + docDir.toAbsolutePath() + "' does not exist or is not readable, please check the path");
+          System.exit(1);
+        }
+    	
+    	Indexer indexer = new Indexer(indexDir,docDir);
+    }
+}
