f70358eddb7d770f58ea590210da92266fb0bfcf
Nirei Orange <nireiorange@gmail.com>
2017-03-15 14:36:19 +0100

    Esqueleto MostsimilarIndexer.

--- mri-indexer/src/main/java/es/udc/fic/mri_indexer/MostsimilarIndexer.java
+++ mri-indexer/src/main/java/es/udc/fic/mri_indexer/MostsimilarIndexer.java
@@ -1,18 +1,22 @@
 package es.udc.fic.mri_indexer;
 
 import java.nio.file.Path;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
 
 public class MostsimilarIndexer {
 	public enum SimilarField {
-		BODY_FIELD,
-		TITLE_FIELD
+		BODY_FIELD, TITLE_FIELD
 	}
-	
+
 	private final Path indexIn;
 	private final Path indexOut;
 	private final SimilarField field;
 	private final int hilos;
-
 
 	public MostsimilarIndexer(Path indexIn, Path indexOut, SimilarField field, int hilos) {
 		this.indexIn = indexIn;
@@ -20,22 +24,62 @@ public class MostsimilarIndexer {
 		this.field = field;
 		this.hilos = hilos;
 	}
-	
+
 	public void recreate() {
-		
-	}
-	
-	private class RunnableMostsimilar implements Runnable {
-		
-		RunnableMostsimilar() {
+		Executors.newFixedThreadPool(hilos);
+		for(;;) {
 			
+		}
+	}
+
+	private class RunnableMostsimilar implements Runnable {
+
+		private final IndexReader reader;
+		private final IndexWriter writer;
+		private final int startId;
+		private final int docAmount;
+		private final int nterms;
+
+		RunnableMostsimilar(IndexReader reader, IndexWriter writer, int startId, int docAmount, int nterms) {
+			this.reader = reader;
+			this.writer = writer;
+			this.startId = startId;
+			this.docAmount = docAmount;
+			this.nterms = nterms;
 		}
 
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
 			
+			for(int i=0; i<docAmount; i++) {
+				Document doc = getDoc(i);
+				// if() // if doc is deleted
+				// continue;
+				String[] terms = getBestNTerms(doc);
+				List<Document> candidates = findDocsWithThisTerms(terms);
+				Document mostSimilar = findMostSimilar(doc, candidates);
+				reindex(doc, mostSimilar);
+			}
 		}
 		
+		private Document getDoc(int index) {
+			return null;
+		}
+		
+		private String[] getBestNTerms(Document doc) {
+			return null;
+		}
+		
+		private List<Document> findDocsWithThisTerms(String[] terms) {
+			return null;
+		}
+		
+		private Document findMostSimilar(Document doc, List<Document> candidates) {
+			return doc;
+		}
+		
+		private void reindex(Document doc, Document mostSimilar) {
+			
+		}
 	}
 }
--- mri-indexer/src/main/java/es/udc/fic/mri_indexer/SearchTfIdf.java
+++ mri-indexer/src/main/java/es/udc/fic/mri_indexer/SearchTfIdf.java
@@ -30,59 +30,16 @@ public class SearchTfIdf {
 		this.field = field;
 		this.numeroTop = top;
 		this.poor = poor;
-		
+
 	}
 
 	public void searching() {
 		try {
 			IndexReader reader;
 			reader = DirectoryReader.open(FSDirectory.open(indexFilePath));
-			System.out.println("Numero de documentos: "+ reader.numDocs());
+			System.out.println("Numero de documentos: " + reader.numDocs());
 			N = reader.numDocs();
-			Fields fields = MultiFields.getFields(reader);
-			Terms terms = fields.terms(field);
-			TermsEnum termsEnum = terms.iterator();
-			List<String> topDocs = new ArrayList<String>();
-			while (termsEnum.next() != null) {
-				// termsEnum.next();
-				// OBTENER IDF
-				int df_T = termsEnum.docFreq();
-				double idf = Math.log(N / df_T);
-
-				// OBTENER string campo
-				BytesRef nombre = termsEnum.term();
-				String nombrestring = nombre.utf8ToString();
-				String termino;
-
-				// OBTENER POSTINNGS
-				PostingsEnum lista;
-				lista = termsEnum.postings(null, PostingsEnum.FREQS);
-
-				if (lista != null) {
-					int docx;
-					while ((docx = lista.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
-
-						double tf_docOrigen; // Tf en el documento
-						double tf_doc; // Calculo de tf con log
-						// OBTENEMOS EL TF
-						tf_doc = lista.freq();
-						tf_docOrigen = tf_doc;
-						if (tf_doc > 0) {
-							tf_doc = 1 + Math.log(tf_doc);
-
-						} else
-							tf_doc = 0;
-						
-						// Calculamos TFIDF
-						double tfidf = tf_doc * idf;
-						// Añadimos los valores a la lista.
-						termino = "TF*IDF: " +   tfidf +" ( docid: " + docx + ", termino: " + nombrestring + " )" + " tf: " + tf_docOrigen + " idf: " + idf;
-						topDocs.add(termino);
-
-					}
-				}
-
-			}
+			List<String> topDocs = calcTopTerms(reader);
 			Collections.sort(topDocs);
 			if (!poor) {
 				Collections.reverse(topDocs);
@@ -98,5 +55,54 @@ public class SearchTfIdf {
 			e.printStackTrace();
 		}
 
+	}
+
+	private List<String> calcTopTerms(IndexReader reader) throws IOException {
+		Fields fields = MultiFields.getFields(reader);
+		Terms terms = fields.terms(field);
+		TermsEnum termsEnum = terms.iterator();
+		List<String> topDocs = new ArrayList<String>();
+		while (termsEnum.next() != null) {
+			// termsEnum.next();
+			// OBTENER IDF
+			int df_T = termsEnum.docFreq();
+			double idf = Math.log(N / df_T);
+
+			// OBTENER string campo
+			BytesRef nombre = termsEnum.term();
+			String nombrestring = nombre.utf8ToString();
+			String termino;
+
+			// OBTENER POSTINNGS
+			PostingsEnum lista;
+			lista = termsEnum.postings(null, PostingsEnum.FREQS);
+
+			if (lista != null) {
+				int docx;
+				while ((docx = lista.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
+
+					double tf_docOrigen; // Tf en el documento
+					double tf_doc; // Calculo de tf con log
+					// OBTENEMOS EL TF
+					tf_doc = lista.freq();
+					tf_docOrigen = tf_doc;
+					if (tf_doc > 0) {
+						tf_doc = 1 + Math.log(tf_doc);
+
+					} else
+						tf_doc = 0;
+
+					// Calculamos TFIDF
+					double tfidf = tf_doc * idf;
+					// Añadimos los valores a la lista.
+					termino = "TF*IDF: " + tfidf + " ( docid: " + docx + ", termino: " + nombrestring + " )" + " tf: "
+							+ tf_docOrigen + " idf: " + idf;
+					topDocs.add(termino);
+
+				}
+			}
+		}
+
+		return topDocs;
 	}
 }
